8e63b52fc945b66061b06a6ccd8b5a29271a12df
hussain-shariff <hussainowaiz070@gmail.com>
2023-04-12 22:11:43 +0530

    object prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import Greet from "./components/Greet"
+import Person from "./components/Person"
 
 function App() {
 
+  const personName = {
+    first : 'Bruce',
+    last : 'Wayne'
+  }
+
   return (
-    <div className="App">
+    <div className="App text-center">
       <Greet
         name = "Hussain"
         age={22}/>
+      <Person
+        name={personName}/>
     </div>
   )
 }
